11ea61b69da06c4fc4e307dbe49bead9ed85c8fe
sabip@infisconsultoria.com.br <sabip@infisconsultoria.com.br>
2020-11-30 18:51:16 -0300

    Adicionando opção de logout e limitando o acesso de usuários não autenticados.

--- 04Proj/quarto-projeto-udemy/src/hoc/Layout/Layout.js
+++ 04Proj/quarto-projeto-udemy/src/hoc/Layout/Layout.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+
 import classes from "./Layout.module.css";
 import Aux from "../Auxiliary/Auxiliary";
 import Toolbar from "../../components/Navigation/Toolbar/Toolbar";
@@ -10,7 +12,7 @@ class Layout extends Component {
   };
 
   sideDrawerClosedHandler = () => {
-    this.setState({showSideDrawer: false});
+    this.setState({ showSideDrawer: false });
   };
 
   sideDrawerToggleHandler = (prevState) => {
@@ -19,19 +21,28 @@ class Layout extends Component {
     });
   };
 
-  render () {
-    return(
+  render() {
+    return (
       <Aux>
-        <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler}/>
-        <SideDrawer 
-          open={this.state.showSideDrawer} 
-          closed={this.sideDrawerClosedHandler}/>
+        <Toolbar 
+        isAuth={this.props.isAuthenticated}
+        drawerToggleClicked={this.sideDrawerToggleHandler} />
+        <SideDrawer
+          isAuth={this.props.isAuthenticated}
+          open={this.state.showSideDrawer}
+          closed={this.sideDrawerClosedHandler} />
         <main className={classes.Content}>
-        {this.props.children}
+          {this.props.children}
         </main>
       </Aux >
     );
   };
 };
 
-export default Layout;
+const mapStateToProps = state => {
+  return {
+    isAuthenticated: state.auth.token !== null
+  };
+};
+
+export default connect(mapStateToProps)(Layout);
